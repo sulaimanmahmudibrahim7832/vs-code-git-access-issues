027132d885eab0611fc740806caeae8eb7b5ba43
sulaiman mahmud ibrahim <sulaimanamahudibrahim@gmail.com>
2026-07-12 15:28:39 +0100

    System reconstruction and re engineering approach

--- account-js/login.js
+++
@@ -1,5 +0,0 @@
-//console.log("hello-world");
-// inputs and divs
-const notification = document.querySelector(".notification-container");
-// hide notification container 
-notification.style.display = "none";
