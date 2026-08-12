b08aa36f86d76f71b143aa4822bd140248a0ba29
sulaiman mahmud ibrahim <sulaimanamahudibrahim@gmail.com>
2026-08-12 16:33:42 +0100

    admin dashboard (edit profile,change paswword activation)

--- Administrator/administrator-js/admin-ui.js
+++ Administrator/administrator-js/admin-ui.js
@@ -13,41 +13,112 @@ const editProfBtn = document.querySelector(".edit-profile-button");
 const changePsBtn = document.querySelector(".change-password-button");
 const logOutBtn = document.querySelector(".logout-button");
 const nameBtn = document.querySelector(".profile-name-save-button");
-const nameBack = document.querySelector(".prifile-name-back-button");
+const BackBtn = document.querySelector(".prifile-name-back-button");
 const delAcc = document.querySelector(".profile-delete-account-button");
-const fullNameSave = document.querySelector(".personalisatiob-save-button");
-const fullNameBack = document.querySelector(".personalisation-back");
 const paswSave = document.querySelector(".password-save");
 const paswBack = document.querySelector(".password-back")
 
 //console.log(editProfBtn, changePsBtn, logOutBtn, nameBtn, nameBack, delAcc, fullNameBack, fullNameSave, paswBack, paswBack);
-// buttons 
+// buttons
+const profileFrom = document.querySelector(".profile-form");
+const profileCard = document.querySelector(".profile-details");
+const passwordCard = document.querySelector(".password-card");
 
-const cards = [user_info, dashboard, create_exam, set_schedule, view_result, recent_activities];
-let initial_ = adminMenu[0].classList.add("active-button");
-let initial_card = user_info.style;
 
-cards.forEach(card => {
-  card.style.display = "none";
-});
-initial_card.display = "grid";
-adminMenu.forEach(menu => {
-  menu.addEventListener("click", () => {
-    adminMenu.forEach(btn => {
-      btn.classList.remove("active-button");
-    });
-    menu.classList.add("active-button");
-    cards.forEach(card => {
-      card.style.display = "none";
-    });
-    for (let i = 0; i < adminMenu.length; i++){
-  if (adminMenu[i].classList.contains("active-button")) {
-    cards[i].style.display = "grid";
+// buttons Associate card
+
+
+let editState;
+let backState;
+let passwordState;
+let backPState;
+profileFrom.style.display = "none";
+passwordCard.style.display = "none";
+editProfBtn.addEventListener("click", () => {
+  editState = true;
+  backState = false;
+
+  if (editState) {
+    profileCard.style.display = "none";
+    profileFrom.style.display = "block";
+
+  } else if (backState) {
+    profileCard.style.display = "block";
+    profileFrom.style.display = "none";
   }
- 
-}
-  });
+
 });
+BackBtn.addEventListener("click", () => {
+  editState = false;
+  backState = true;
+      if (editState) {
+      profileCard.style.display = "none";
+      profileFrom.style.display = "block";
+    } else if (backState) {
+      profileCard.style.display = "block";
+      profileFrom.style.display = "none";
+    }
+});
+changePsBtn.addEventListener("click", () => {
+  passwordState = true;
+  backPState = false;
+
+
+  if (passwordState) {
+    profileCard.style.display = "none";
+    passwordCard.style.display = "block";
+
+
+  } else if (backPState) {
+    profileCard.style.display = "block";
+    passwordCard.style.display = "none";
+  }
+
+
+});
+
+paswBack.addEventListener("click", () => {
+   passwordState = false;
+ backPState = true;
+ if (passwordState) {
+   profileCard.style.display = "none";
+   passwordCard.style.display = "block";
+ } else if (backPState) {
+   profileCard.style.display = "block";
+   passwordCard.style.display = "none";
+ }
+});
+
+//
+
+function controlUI() {
+  const cards = [user_info, dashboard, create_exam, set_schedule, view_result, recent_activities];
+  let initial_ = adminMenu[0].classList.add("active-button");
+  let initial_card = user_info.style;
+
+  cards.forEach(card => {
+    card.style.display = "none";
+  });
+  initial_card.display = "grid";
+  adminMenu.forEach(menu => {
+    menu.addEventListener("click", () => {
+      adminMenu.forEach(btn => {
+        btn.classList.remove("active-button");
+      });
+      menu.classList.add("active-button");
+      cards.forEach(card => {
+        card.style.display = "none";
+      });
+      for (let i = 0; i < adminMenu.length; i++) {
+        if (adminMenu[i].classList.contains("active-button")) {
+          cards[i].style.display = "grid";
+        }
+ 
+      }
+    });
+  });
+}
+controlUI();
 
 
 
