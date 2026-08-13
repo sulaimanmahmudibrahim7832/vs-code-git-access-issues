d013fb067c7027286fb1c5d94798e14a3bc9d7a9
sulaiman mahmud ibrahim <sulaimanamahudibrahim@gmail.com>
2026-08-13 18:43:44 +0100

    left_nav renderation standardisation

--- Student/student-js/student-ui-js.js
+++ Student/student-js/student-ui-js.js
@@ -2,6 +2,15 @@
 
 const menu = document.querySelectorAll(".menu div");
 let initial_ = menu[0].classList.add("active-button");
+
+
+
+
+renderLeftNav(initial_,menu);
+
+
+function renderLeftNav(intitial_,menu) {
+
 menu.forEach(tab => {
   tab.addEventListener("click", btn => {
     menu.forEach(tab => {
@@ -10,3 +19,4 @@ menu.forEach(tab => {
     tab.classList.add("active-button");
   });
 });
+}
